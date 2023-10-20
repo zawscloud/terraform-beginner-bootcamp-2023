32f43f1d3c61758cb2335906ceec63b3266fc3e8
Jared <83472934+zawscloud@users.noreply.github.com>
2023-10-20 11:51:15 -0400

    #29 Create cdn and serve s3 bucket from cdn (#30)

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 terraform {
-#   cloud {
-#     organization = "zawscloud-terraform-bootcamp"
+  #   cloud {
+  #     organization = "zawscloud-terraform-bootcamp"
 
-#     workspaces {
-#       name = "terra-house-1"
-#     }
-#   }
+  #     workspaces {
+  #       name = "terra-house-1"
+  #     }
+  #   }
 
 }
 
 module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath= var.index_html_filepath
-  error_html_filepath= var.error_html_filepath
+  source              = "./modules/terrahouse_aws"
+  user_uuid           = var.user_uuid
+  bucket_name         = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
 }
