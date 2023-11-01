7935a19244264e3d7520a484ed8dea4cd18b96ee
Jared <bleahzj@gmail.com>
2023-11-01 17:36:47 +0000

    #40 Document terratowns mock server, add validation for inclusion

--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -3,22 +3,55 @@ require 'json'
 require 'pry'
 require 'active_model'
 
+# We will mock having a state or database for this development server.
+# by setting a global variable. You will never use a global variable
+# in a production server
 $home = {}
 
+# This is a ruby class that includes validations from ActiveRecord.
+# This will represent our Home resources as a ruby object.
 class Home
+  # ActiveModel is part of Ruby on Rails
+  # it is used as an ORM. IT has a module within ActiveModel that provides validations.
+  # The production Terratowns server is rails and uses 
+  # very similar and in most cases identical validation
+  # https://guides.rubyonrails.org/active_model_basics.html
+  # https://guides.rubyonrails.org/active_record_validations.html
   include ActiveModel::Validations
+
+  # create some virtual attributes to store on this object
+  # this will set a getter and setter
+  # eg.
+  # home = new Home()
+  # home.town = 'hello' # setter
+  # home.town() = # getter
   attr_accessor :town, :name, :description, :domain_name, :content_version
 
-  validates :town, presence: true
+  # gamers-groto
+  # cooker-cove
+  validates :town, presence: true, inclusion: { in: [
+    'melomaniac-mansion',
+    'cooker-cove',
+    'video-valley',
+    'the-nomad-pad',
+    'gamers-grotto'
+  ] }
+  # visible to all users
   validates :name, presence: true
+  # visible to all users
   validates :description, presence: true
+  # we want to lock this down to only be from cloudfront
   validates :domain_name, 
     format: { with: /\.cloudfront\.net\z/, message: "domain must be from .cloudfront.net" }
     # uniqueness: true, 
-
+  
+    # content version has to be an integer
+    # we will make sure it is an incremental version in the controller. 
   validates :content_version, numericality: { only_integer: true }
 end
 
+# We are extending a class from Sinatra::Base to
+# turn this generic class to utilize the sinatra web-framework 
 class TerraTownsMockServer < Sinatra::Base
 
   def error code, message
@@ -39,6 +72,7 @@ class TerraTownsMockServer < Sinatra::Base
     end
   end
 
+  # return a hard coded access token
   def x_access_code
     '9b49b3fb-b8e9-483c-b703-97ba88eef8e0'
   end
@@ -49,19 +83,26 @@ class TerraTownsMockServer < Sinatra::Base
 
   def find_user_by_bearer_token
     auth_header = request.env["HTTP_AUTHORIZATION"]
+    # check if the autorization header exists?
     if auth_header.nil? || !auth_header.start_with?("Bearer ")
       error 401, "a1000 Failed to authenicate, bearer token invalid and/or teacherseat_user_uuid invalid"
     end
 
+    # Does the token match the one in our database?
+    # if we cant find it that return an error or if it doesn't match
+    # code = access_code = token
     code = auth_header.split("Bearer ")[1]
     if code != x_access_code
       error 401, "a1001 Failed to authenicate, bearer token invalid and/or teacherseat_user_uuid invalid"
     end
 
+    # was there a user_uuid in the body payload json?
     if params['user_uuid'].nil?
       error 401, "a1002 Failed to authenicate, bearer token invalid and/or teacherseat_user_uuid invalid"
     end
 
+    # the code and the user_uuid should be matching for user
+    # in rails: User.find_by access_code: code, user_uuid: user_uuid
     unless code == x_access_code && params['user_uuid'] == x_user_uuid
       error 401, "a1003 Failed to authenicate, bearer token invalid and/or teacherseat_user_uuid invalid"
     end
@@ -69,8 +110,9 @@ class TerraTownsMockServer < Sinatra::Base
 
   # CREATE
   post '/api/u/:user_uuid/homes' do
-    ensure_correct_headings
-    find_user_by_bearer_token
+    ensure_correct_headings()
+    find_user_by_bearer_token()
+    # puts will print tothe terminal similar to a print or consol.lor
     puts "# create - POST /api/homes"
 
     begin
@@ -185,4 +227,5 @@ class TerraTownsMockServer < Sinatra::Base
   end
 end
 
+# This is what will run the server.
 TerraTownsMockServer.run!
