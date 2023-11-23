3b0665ca7273de9696d869d308c97727b5898e3a
Jared <83472934+zawscloud@users.noreply.github.com>
2023-11-23 08:11:14 -0500

    42 terratowns provider (#46)
    * #42 WIP setting up the skeleton for creating a custom tf provider
    
    * #42 implement tf provider block in terraform.
    
    * #42 step skeleton for resources.
    
    * #42 finish implementation of terraform provider

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,6 +5,4 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:FA/GA8K102vmHj1dzLplT+VIufAIyNpDZESq91dPN0o=",
-  ]
 }
--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
 
 }
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
   token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -28,3 +28,17 @@ provider "terratowns" {
 #   content_version     = var.content_version
 #   assets_path = var.assets_path
 # }
+
+resource "terratowns_home" "home" {
+  name = "How to play Arcunum in 2023! to 2024!"
+  description = <<DESCRIPTION
+Arcanum is a game from 2001 that shipped with alot of bugs.
+Modders have removed all the originals making this game really fun to play
+(despite the old looking graphics). This is my guide that will show you how to play
+arcanum without spoiling the plot
+  DESCRIPTION
+  # domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdighz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
